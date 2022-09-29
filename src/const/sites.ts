0b8d5c7d523f9a4615e4239f02b79948f18a05c3
overthestream <njhnjh02@naver.com>
2022-09-29 16:23:49 +0900

    feat: frontier, 보류

--- src/const/sites.ts
+++ src/const/sites.ts
@@ -1,5 +1,7 @@
-import { siteInfo } from '@/interface/site'
-export const sites: siteInfo[] = [
-  { host: "https://info.korea.ac.kr", description: "korea univ college of info", urls: ["/info/board/notice_under.do"] }
-]
+import { siteInfo } from '@/interface/site';
 
+const sites: siteInfo[] = [
+  { host: 'https://info.korea.ac.kr', description: 'korea univ college of info', urls: ['/info/board/notice_under.do'] },
+];
+
+export default sites;
